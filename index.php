5773652ae28863f17ef2b45036ef5714b9b1910b
DokeshiLab1_computer2 <isack_santana@estudante.sesisenai.org.br>
2026-08-18 12:11:51 -0300

    add - ui cadastro pratos

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
 include "infra/conexao.php"
-$cadastros = mysqli_query($conexao, "SELECT * FROM cadastros");
-$pratos = mysqli_query($conexao, "SELECT * FROM pratos");
 
 ?>
 
@@ -16,12 +14,37 @@ $pratos = mysqli_query($conexao, "SELECT * FROM pratos");
 <body>
     <h1>Hey kiddo</h1>
     <main>
+        <h2>Usuario</h2>
         <form action="public/cadastrar.php" method="POST">
             <label for="nome">Nome:</label>
-            <input type="text" name="titulo">
+            <input type="text" name="nomeUsuario">
             <br>
             <label for="Email">Email:</label>
-            <input type="text" name="autor">
+            <input type="email" name="emailUsuario">
+            <br>
+            <button type="submit">Cadastrar</button>
+        </form>
+        <h2>Pratos</h2>
+        <form action="public/cadastrar.php" method="POST">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nomePrato">
+            <br>
+            <label for="valor">Valor:</label>
+            <input type="number" name="valor">
+            <br>
+            <label for="categoria">Categoria:</label>
+            <br>
+            <input type="radio" id="entrada" name="categoria" value="entrada">
+            <label for="categoria">Entrada</label>
+            <br>
+            <input type="radio" id="pratoPrincipal" name="categoria" value="pratoPrincipal">
+            <label for="categoria">Prato Principal</label>
+            <br>
+            <input type="radio" id="acompanhamento" name="categoria" value="acompanhamento">
+            <label for="categoria">Acompanhamento</label>
+            <br>
+            <input type="radio" id="sobremesa" name="categoria" value="sobremesa">
+            <label for="categoria">Sobremesa</label>
             <br>
             <button type="submit">Cadastrar</button>
         </form>
