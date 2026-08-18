b2f82994a68ee501fdd8c947eef38357938d1fdc
DokeshiLab1_computer2 <isack_santana@estudante.sesisenai.org.br>
2026-08-18 08:26:24 -0300

    add - foreign keys db

--- database/database.sql
+++ database/database.sql
@@ -1,14 +1,16 @@
 IF NOT EXIST CREATE DATABASE menu_db
 USE menu_db
 
+CREATE TABLE cadastros
+cadastroId int(20) AUTO_INCREMENT PRIMARY KEY,
+cadastroNome varchar(50),
+cadastroEmail varchar(50);
+
 CREATE TABLE pratos
 pratoId int(20) AUTO_INCREMENT PRIMARY KEY,
 pratoNome varchar(50),
 pratoValor int(20),
 pratoDescrição varchar(150),
-pratoCategoria varchar(50);
-
-CREATE TABLE cadastros
-cadastroId int(20) AUTO_INCREMENT PRIMARY KEY,
-cadastroNome varchar(50),
-cadastroEmail varchar(50);
+pratoCategoria varchar(50),
+FOREIGN KEY (cadastroId) REFERENCES cadastros(cadastroId),
+FOREIGN KEY (cadastroNome) REFERENCES cadastros(cadastroNome);
