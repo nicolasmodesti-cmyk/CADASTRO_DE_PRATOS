13d9510b83ba2fb4a4faef9208e5df49273cc8c1
DokeshiLab1_computer2 <isack_santana@estudante.sesisenai.org.br>
2026-08-18 08:23:02 -0300

    update - db file, just some more text

--- db.sql
+++ db.sql
@@ -1,4 +1,14 @@
 IF NOT EXIST CREATE DATABASE menu_db
 USE menu_db
+
 CREATE TABLE pratos
+pratoId int(20) AUTO_INCREMENT PRIMARY KEY,
+pratoNome varchar(50),
+pratoValor int(20),
+pratoDescrição varchar(150),
+pratoCategoria varchar(50);
+
 CREATE TABLE cadastros
+cadastroId int(20) AUTO_INCREMENT PRIMARY KEY,
+cadastroNome varchar(50),
+cadastroEmail varchar(50);
